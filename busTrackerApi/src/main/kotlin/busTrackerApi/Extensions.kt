465e7d540ae650cdab7704e89ee68f89bc2de7cd
xBaank <robertobl022@gmail.com>
2023-06-24 18:40:19 +0200

    Cleanup

--- busTrackerApi/src/main/kotlin/busTrackerApi/Extensions.kt
+++ busTrackerApi/src/main/kotlin/busTrackerApi/Extensions.kt
@@ -2,7 +2,6 @@ package busTrackerApi
 
 import com.auth0.jwt.JWTCreator
 import com.toxicbakery.bcrypt.Bcrypt
-import io.github.reactivecircus.cache4k.Cache
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
@@ -11,9 +10,6 @@ import simpleJson.jObject
 import simpleJson.serialized
 import java.util.*
 import kotlin.time.Duration
-
-inline fun <T : Any, K : Any> Cache<T, K>.getOrPut(key: T, value: () -> K): K =
-    get(key) ?: value().also { put(key, it) }
 
 fun getenvOrThrow(key: String): String =
     System.getenv(key) ?: System.getProperty(key) ?: throw IllegalStateException("Environment variable $key is not set")
@@ -38,21 +34,21 @@ suspend fun PipelineContext<Unit, ApplicationCall>.badRequest(message: String?) 
 }
 
 suspend fun PipelineContext<Unit, ApplicationCall>.unauthorized(message: String?) {
-    val messageResponse = message ?: "Bad Request"
+    val messageResponse = message ?: "Unauthorized"
     val messageResponseJson = errorObject(messageResponse)
 
     call.respondText(messageResponseJson, ContentType.Application.Json, HttpStatusCode.Unauthorized)
 }
 
 suspend fun PipelineContext<Unit, ApplicationCall>.notFound(message: String?) {
-    val messageResponse = message ?: "Bad Request"
+    val messageResponse = message ?: "Not found"
     val messageResponseJson = errorObject(messageResponse)
 
     call.respondText(messageResponseJson, ContentType.Application.Json, HttpStatusCode.NotFound)
 }
 
 suspend fun PipelineContext<Unit, ApplicationCall>.conflict(message: String?) {
-    val messageResponse = message ?: "Bad Request"
+    val messageResponse = message ?: "Conflict"
     val messageResponseJson = errorObject(messageResponse)
 
     call.respondText(messageResponseJson, ContentType.Application.Json, HttpStatusCode.Conflict)
