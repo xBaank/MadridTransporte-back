61777008cc2e2039cea9f551d56fa75dba6140f2
Roberto Blázquez <elroby444@gmail.com>
2023-08-04 01:01:10 +0200

    Cleanup

--- busTrackerApi/src/main/kotlin/busTrackerApi/utils/Environment.kt
+++ busTrackerApi/src/main/kotlin/busTrackerApi/utils/Environment.kt
@@ -1,7 +1,4 @@
 package busTrackerApi.utils
 
-fun getenvOrThrow(key: String): String =
-    System.getenv(key) ?: System.getProperty(key) ?: throw IllegalStateException("Environment variable $key is not set")
-
 fun getenvOrNull(key: String): String? =
     System.getenv(key) ?: System.getProperty(key) ?: null
