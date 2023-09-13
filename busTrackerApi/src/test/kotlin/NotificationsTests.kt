549835512169c542398b30e96bde334959484b70
Roberto Blázquez <elroby444@gmail.com>
2023-09-13 02:44:03 +0200

    WIP

--- busTrackerApi/src/test/kotlin/NotificationsTests.kt
+++ busTrackerApi/src/test/kotlin/NotificationsTests.kt
@@ -1,7 +1,9 @@
 import arrow.core.right
 import busTrackerApi.routing.stops.*
 import com.google.api.core.ApiFuture
+import com.google.firebase.ErrorCode
 import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingException
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.mockk.*
@@ -40,11 +42,14 @@ class NotificationsTest {
             delayTime = 5.seconds
             val functionMocked: StopTimesF = { testStopTimes.right() }
             val firebaseMessaging = mockk<FirebaseMessaging>()
+            val future = mockk<ApiFuture<String>>()
             mockkStatic(FirebaseMessaging::class)
             mockkStatic(::getFunctionByCodMode)
+            every { future.isDone } returns true
+            every { future.get() } returns "MessageId"
             coEvery { getFunctionByCodMode(any()) } returns functionMocked.right()
             every { FirebaseMessaging.getInstance() } returns firebaseMessaging
-            every { firebaseMessaging.sendAsync(any()) } returns mockk<ApiFuture<String>>()
+            every { firebaseMessaging.sendAsync(any()) } returns future
 
             val body = jObject {
                 "deviceToken" += "token"
@@ -68,7 +73,7 @@ class NotificationsTest {
                     "stopCode" += subscription.stopcode
                 }.serialized())
             }
-            
+
             delay(delayTime)
 
             val unsubscribeResponse = client.post(subscription.url + "/unsubscribe") {
@@ -79,5 +84,55 @@ class NotificationsTest {
             subscribedResponse.status.isSuccess().shouldBe(true)
             subscriptionsResponse.status.isSuccess().shouldBe(true)
             unsubscribeResponse.status.isSuccess().shouldBe(true)
+        }
+
+    @ParameterizedTest
+    @EnumSource(Subscriptions::class)
+    fun `should subscribe to line times and unsubscribe on error`(subscription: Subscriptions) =
+        testApplicationBusTracker {
+            delayTime = 5.seconds
+            val functionMocked: StopTimesF = { testStopTimes.right() }
+            val firebaseMessaging = mockk<FirebaseMessaging>()
+            val firebaseMessagingException = mockk<FirebaseMessagingException>()
+            mockkStatic(FirebaseMessaging::class)
+            mockkStatic(::getFunctionByCodMode)
+            coEvery { getFunctionByCodMode(any()) } returns functionMocked.right()
+            every { FirebaseMessaging.getInstance() } returns firebaseMessaging
+            every { firebaseMessagingException.errorCode } answers { ErrorCode.NOT_FOUND }
+            every { firebaseMessagingException.message } answers { "" }
+            every { firebaseMessagingException.stackTrace } answers { emptyArray() }
+            every { firebaseMessagingException.cause } answers { null }
+            every { firebaseMessagingException.suppressed } answers { emptyArray() }
+            every { firebaseMessaging.sendAsync(any()) } throws firebaseMessagingException
+
+            val body = jObject {
+                "deviceToken" += "token"
+                "subscription" += jObject {
+                    "stopCode" += subscription.stopcode
+                    "lineDestination" += jObject {
+                        "line" += "8"
+                        "destination" += "asd"
+                        "codMode" += 8
+                    }
+                }
+            }.serialized()
+
+            val subscribedResponse = client.post(subscription.url + "/subscribe") {
+                setBody(body)
+            }
+
+            delay(delayTime)
+
+            val subscriptionsResponse = client.post(subscription.url + "/subscription") {
+                setBody(jObject {
+                    "deviceToken" += "token"
+                    "stopCode" += subscription.stopcode
+                }.serialized())
+            }
+
+
+            verify(timeout = delayTime.toLong(DurationUnit.MILLISECONDS)) { firebaseMessaging.sendAsync(any()) }
+            subscribedResponse.status.isSuccess().shouldBe(true)
+            subscriptionsResponse.status.shouldBe(HttpStatusCode.NotFound)
         }
 }
