04c33fc6de65e702e0b6bc47241636f065624da4
Roberto Blázquez <elroby444@gmail.com>
2023-10-14 21:25:06 +0200

    Cleanup

--- busTrackerApi/src/main/kotlin/busTrackerApi/extensions/CollectionExtensions.kt
+++ busTrackerApi/src/main/kotlin/busTrackerApi/extensions/CollectionExtensions.kt
@@ -4,11 +4,6 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 
-suspend fun <A> Collection<A>.onEachAsync(f: suspend (A) -> Unit) = coroutineScope {
-    forEachAsync { f(it) }
-    this@onEachAsync
-}
-
 suspend fun <A> Collection<A>.forEachAsync(f: suspend (A) -> Unit) = coroutineScope {
     map { launch { f(it) } }.joinAll()
 }
