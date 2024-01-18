1dd64dca5df21de9872c445c0baaa63d9c1f8823
Roberto Blázquez <elroby444@gmail.com>
2024-01-18 18:37:47 +0100

    WIP

--- busTrackerApi/src/main/kotlin/busTrackerApi/routing/stops/train/TrainStopsController.kt
+++ busTrackerApi/src/main/kotlin/busTrackerApi/routing/stops/train/TrainStopsController.kt
@@ -1,16 +1,17 @@
 package busTrackerApi.routing.stops.train
 
 import arrow.core.continuations.either
+import busTrackerApi.config.EnvVariables.timeoutSeconds
 import busTrackerApi.db.getCoordinatesByStopCode
 import busTrackerApi.db.getIdByStopCode
 import busTrackerApi.db.getStopNameById
 import busTrackerApi.exceptions.BusTrackerException.InternalServerError
 import busTrackerApi.exceptions.BusTrackerException.NotFound
+import busTrackerApi.extensions.await
 import busTrackerApi.extensions.bindJson
 import busTrackerApi.extensions.post
 import busTrackerApi.routing.stops.train.cano.canoHttpClient
 import crtm.utils.getStopCodeFromFullStopCode
-import ru.gildor.coroutines.okhttp.await
 import simpleJson.deserialized
 import simpleJson.jObject
 
@@ -19,37 +20,50 @@ suspend fun getTrainRoutedTimesResponse(fullStopCode: String) = either {
     val stopName = getStopNameById(stopInfoStationCode).bind()
     val coordinates = getCoordinatesByStopCode(fullStopCode).bind()
 
-    val response = canoHttpClient.post(
-        "https://circulacion.api.adif.es/portroyalmanager/secure/circulationpaths/departures/traffictype/",
-        jObject {
-            "commercialService" += "YES"
-            "commercialStopType" += "YES"
-            "page" += jObject {
-                "pageNumber" += 0
+    var tries = 3
+    do {
+        val response = canoHttpClient.post(
+            "https://circulacion.api.adif.es/portroyalmanager/secure/circulationpaths/departures/traffictype/",
+            jObject {
+                "commercialService" += "YES"
+                "commercialStopType" += "YES"
+                "page" += jObject {
+                    "pageNumber" += 0
+                }
+                "stationCode" += stopInfoStationCode
+                "trafficType" += "CERCANIAS"
+            },
+            contentType = null,
+            headers = mapOf(
+                "User-Key" to "f4ce9fbfa9d721e39b8984805901b5df",
+                "Host" to "circulacion.api.adif.es",
+                "User-Agent" to "okhttp/4.10.0",
+                "Connection" to "Close"
+            )
+        ).await(timeoutSeconds)
+
+        if (response == null) {
+            tries--
+            continue
+        }
+
+
+        response.use {
+            if (it.code == 404) shift<Nothing>(NotFound())
+            if (!it.isSuccessful) {
+                tries--
+                return@use
             }
-            "stationCode" += stopInfoStationCode
-            "trafficType" += "CERCANIAS"
-        },
-        contentType = null,
-        headers = mapOf(
-            "User-Key" to "f4ce9fbfa9d721e39b8984805901b5df",
-            "Host" to "circulacion.api.adif.es",
-            "User-Agent" to "okhttp/4.10.0",
-            "Connection" to "Close"
-        )
-    ).await()
 
+            val json = it.body?.string()?.deserialized()?.bindJson() ?: shift<Nothing>(InternalServerError())
 
-    response.use {
-        if (it.code == 404) shift<Nothing>(NotFound())
-        if (!it.isSuccessful) return@either createTrainFailedTimes(
-            name = stopName,
-            coordinates = coordinates,
-            stopCode = getStopCodeFromFullStopCode(fullStopCode)
-        )
+            parseTrainToStopTimes(json, coordinates, stopName, getStopCodeFromFullStopCode(fullStopCode)).bind()
+        }
+    } while (tries > 0)
 
-        val json = it.body?.string()?.deserialized()?.bindJson() ?: shift<Nothing>(InternalServerError())
-
-        parseTrainToStopTimes(json, coordinates, stopName, getStopCodeFromFullStopCode(fullStopCode)).bind()
-    }
+    createTrainFailedTimes(
+        name = stopName,
+        coordinates = coordinates,
+        stopCode = getStopCodeFromFullStopCode(fullStopCode)
+    )
 }
