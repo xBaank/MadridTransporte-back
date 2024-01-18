7f8379a0798d4173dda568ba6ff4b808752026d1
Roberto Blázquez <elroby444@gmail.com>
2024-01-18 21:44:57 +0100

    Fix tests

--- busTrackerApi/src/main/kotlin/busTrackerApi/routing/stops/train/TrainStopsRouting.kt
+++ busTrackerApi/src/main/kotlin/busTrackerApi/routing/stops/train/TrainStopsRouting.kt
@@ -2,14 +2,15 @@ package busTrackerApi.routing.stops.train
 
 import busTrackerApi.extensions.handle
 import busTrackerApi.routing.stops.alertsConfigF
+import busTrackerApi.routing.stops.subConfigF
 import busTrackerApi.routing.stops.trainRouted.trainCodMode
 import io.ktor.server.routing.*
 
 fun Route.trainStopsRouting() = route("/train") {
-
     get("/{stopCode}/times") {
         handle { getTrainStopsTimes() }
     }
 
+    subConfigF(trainCodMode)
     alertsConfigF(trainCodMode)
 }
