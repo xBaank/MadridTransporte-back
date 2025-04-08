d90b66f966b4499b34ff99b3473f220caa231f7b
Roberto Blázquez <elroby444@gmail.com>
2025-04-08 14:52:25 +0200

    Update pre (#169)
    * Update gradle.yml
    
    * Update dependency io.github.pdvrieze.xmlutil:core to v0.91.0 (#165)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update docker/build-push-action digest to 66147ca (#167)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency io.github.pdvrieze.xmlutil:serialization to v0.91.0 (#166)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update kotlinx-coroutines monorepo to v1.10.2 (#168)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -40,13 +40,13 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:$okhttp_version")
     implementation("io.github.reactivecircus.cache4k:cache4k:0.14.0")
     implementation("io.ktor:ktor-server-compression:$ktor_version")
-    implementation("io.github.pdvrieze.xmlutil:core:0.90.3")
-    implementation("io.github.pdvrieze.xmlutil:serialization:0.90.3")
+    implementation("io.github.pdvrieze.xmlutil:core:0.91.0")
+    implementation("io.github.pdvrieze.xmlutil:serialization:0.91.0")
     implementation("io.ktor:ktor-server-caching-headers:$ktor_version")// https://mvnrepository.com/artifact/com.google.firebase/firebase-messaging
     implementation("com.google.firebase:firebase-admin:9.4.3")
     implementation("com.google.auth:google-auth-library-oauth2-http:1.33.1")
     // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-guava
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.10.2")
     implementation("dev.inmo:krontab:2.7.2")
     // https://mvnrepository.com/artifact/org.jsoup/jsoup
     implementation("org.jsoup:jsoup:1.19.1")
